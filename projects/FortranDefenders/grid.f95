3fa9f552141cb1e1ae517a06b1899f495e49faee
Kyle Godbey <kylegodbey@gmail.com>
2016-07-18 18:15:04 +0100

    Initial commit with baby code

---
+++ projects/FortranDefenders/grid.f95
@@ -0,0 +1,28 @@
+module grid
+	implicit none
+	integer, parameter :: wp=kind(1.0d0)
+	real(wp), parameter :: pi = 3.14159265358979_wp
+	real(wp) :: h,nbox,conv,hbar22m
+	real(wp), allocatable,dimension(:) :: meshpoints
+
+contains
+	subroutine init_params
+	
+	namelist /input/ nbox,h,conv,hbar22m
+	read(5,input)
+
+	end subroutine init_params
+
+
+
+	subroutine init_grids
+	
+	integer :: i
+
+	allocate(meshpoints(nbox))
+	meshpoints = (/ (real(i)*h,i=0,nbox) /)
+	
+	end subroutine init_grids
+
+
+end module grid
