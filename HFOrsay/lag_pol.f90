5ebea1e60ac74fd3c10b392b23f997e897f704a3
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-07-19 18:18:28 +0200

    Working HO radial wf

--- HFOrsay/lag_pol.f90
+++ HFOrsay/lag_pol.f90
@@ -2,24 +2,26 @@
 module lag_pol
 double precision,dimension(:),allocatable::lag_zeros,lag_w
 contains
-subroutine lag_roots(n,pr)
+subroutine lag_roots(n,m,pr)
+use lag
 implicit none
-double precision,external::laguerre
+!double precision,external::laguerre
 double precision::e,x,step,val,x_max,xi
 integer,intent(in)::n
+double precision,intent(in)::m
 logical::t,pr
 integer::i,j
   write(*,*) '**** Laguerre Roots ****'
   open(10,file='roots_lag.dat')
   x = 0.0 ; step = 1.0e-6 ; e = 1.0e-9;x_max=1.0e4
-  t = (laguerre(n,x) > 0)
+  t = (laguerre(n,m,x) > 0)
   j = 0
   allocate(lag_zeros(n))
   allocate(lag_w(n))
   lag_zeros=0.d0
   lag_w=0.d0
   do while (x < x_max)
-    val = laguerre(n,x)
+    val = laguerre(n,m,x)
     if (abs(val) < e) then
       j = j + 1
       if (pr) then
@@ -43,26 +45,12 @@ integer::i,j
   close(10)
   do i=1,n
   xi=lag_zeros(i)
-  lag_w(i)=xi/((n+1)**2*laguerre(n+1,xi)**2)
+  lag_w(i)=xi/((n+1)**2*laguerre(n+1,m,xi)**2)
   enddo
   write(*,*) '** End Laguerre Roots **'
 end subroutine lag_roots
-
-
-
 end module lag_pol
-recursive function laguerre(n,x) result (lag)
-implicit none
-integer::n
-double precision::x,lag
-if (n == 0) then
-    lag = 1
-else if (n == 1) then
-    lag = 1.d0-x
-else
-    lag = ((2*n-1-x)*laguerre(n-1,x)-(n-1)*laguerre(n-2,x))/n
-endif
-end function laguerre
+
 
 
 
