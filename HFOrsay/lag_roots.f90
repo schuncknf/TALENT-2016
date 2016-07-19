9d3da575e387f84b1c82182e49bfaa536af31fb7
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-07-19 15:12:43 +0200

    Working roots of laguerre polynomials computation

--- HFOrsay/lag_roots.f90
+++ HFOrsay/lag_roots.f90
@@ -1,24 +1,35 @@
-subroutine lag_roots(n)
+subroutine lag_roots(n,pr)
 implicit none
 double precision,external::laguerre
-double precision::e,x,step,val
-double precision,dimension(:),allocatable::lag_zero
-logical::t
-integer::i,n
- n=2
- !lag_zero = 0.d0
- x = -1.0 ; step = 1.0e-6 ; e = 1.0e-9
+double precision::e,x,step,val,x_max
+integer,intent(in)::n
+logical::t,pr
+integer::i,j
+  write(*,*) '**** Laguerre Roots ****'
+  open(10,file='roots_lag.dat')
+  x = 0.0 ; step = 1.0e-6 ; e = 1.0e-9;x_max=1.0e4
   t = (laguerre(n,x) > 0)
-  do while (x < 3.0)
+  j = 0
+  do while (x < x_max)
     val = laguerre(n,x)
     if (abs(val) < e) then
-      WRITE(*,"(A,F12.9)") "Root found at x =", x
+      j = j + 1
+      if (pr) then
+      WRITE(*,*) "Laguerre root",j," =", x
+      write(10,*) x
+      endif
       t = .not. t
     else if ((val > 0) .neqv. t) then
-      write(*,"(A,F12.9)") "Root found near x = ", x
+      j = j + 1
+      if (pr) then
+      WRITE(*,*) "Laguerre root",j," =", x
+      write(10,*) x
+      endif
       t = .not. t
     end if
     x = x + step
+    if (j .eq. n) exit
   end do
-!  lag_zero(i) = x
+  close(10)
+  write(*,*) '** End Laguerre Roots **'
 end
