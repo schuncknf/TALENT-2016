0c323f3867d768309034b8408803ec11f9975e06
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-07-19 23:15:54 +0200

    Calculating matrix elements

--- HFOrsay/ho.f90
+++ HFOrsay/ho.f90
@@ -7,11 +7,13 @@ use constants
 use lag_pol
 implicit none
 integer::n,l
-double precision::r,nnorm,norm,xi,wf
+double precision::r,nnorm,norm,xi,wf,expf
 xi = r/bosc
 norm=dsqrt(2**(n+l+2)*fac(n)/(dsqrt(pi)*ffac(2*n+2*l+1)))
 nnorm=norm/bosc**(3.d0/2.d0)
-wf = nnorm*xi**l*exp(-xi**2/2.d0)*laguerre(n,dble(l+1.d0/2.d0),xi**2)
+expf = exp(-(xi**2)*0.5d0)
+wf = nnorm*xi**l*expf*laguerre(n,dble(l+1.d0/2.d0),xi**2)
+!wf = nnorm*xi**l*laguerre(n,dble(l+1.d0/2.d0),xi**2)
 
 end function ho_rad_wf
 end module ho
