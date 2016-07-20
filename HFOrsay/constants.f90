20b33da25642f4d3dfa28f4023e07d7610bf8a0e
maxime mougeot <maxime.mougeot@cern.ch>
2016-07-20 15:54:10 +0100

    added the parameters of the minnesota potential in the constants module

--- HFOrsay/constants.f90
+++ HFOrsay/constants.f90
@@ -3,4 +3,13 @@ implicit none
 double precision,parameter::one = 1.d0,two=2.d0
 double precision,parameter::pi=3.14159265359
 double precision,parameter::bosc=1.d0
+
+!definition of the parameters of the t
+double precision,parameter::kr=1.487d0 !in fm -2
+double precision,parameter::kt=0.639d0 !in fm -2
+double precision,parameter::ks=0.465d0 !in fm -2
+double precision,parameter::v0r=200.00d0 !in MeV
+double precision,parameter::v0t=178.00d0 !in MeV
+double precision,parameter::v0s=91.85d0 !in MeV
+
 end module constants
