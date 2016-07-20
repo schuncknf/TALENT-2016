1a5d8ec6e1999c6b7d70d9949b6b0a21077cc83b
cgonzalezboquera <cgonzalezboquera@ub.edu>
2016-07-20 18:11:01 +0200

    added plot of eigenvectors; changed global variables and main of infinite well potential

--- PeTsBav2/global.f90
+++ PeTsBav2/global.f90
@@ -1,12 +1,20 @@
       module globals
       implicit none
-      integer :: dm=kind(1.d0)
-      real (kind=dm), parameter :: h2m= 20.75, eps=1e-6
-      real (kind=dm) :: R_max=10., R_min=0., R_box=6.,h=0.1,Em
-      integer (kind=dm) :: Nmesh
+      integer, parameter :: dm=kind(1.d0)
+      real (kind=dm) ::h2m= 20.75, epsi=1e-6
+      real (kind=dm) :: R_max=6., R_min=0., R_box=6.,h=0.1,Em, nmfactor
+      integer  :: Nmesh
       ! psi(grid_index)
-      real (kind=dm),allocatable :: Psi(:) 
+      real (kind=dm),allocatable :: Psi(:) , rho(:)
       logical :: comparison= .true., bisloop= .true.
-      real (kind=dm) :: E_minus=0., E_plus=100.
-      Nmesh=nint((R_max-R_min)/h)
+      real (kind=dm) :: E_minus=0., E_plus=100., E_left, E_right
+      integer(kind=dm) :: numnodes ! the number of excited state, that has to be the same as number of nodes in wavefunction
+      integer(kind=dm) :: cnodes ! nodes of wavefuntion 
+      integer(kind=dm) :: i
+     ! real(kind=dm) :: vpot
+      real(kind=dm),allocatable :: k_sq(:)
+      real(kind=dm) :: x  
       end module globals
+
+
+
