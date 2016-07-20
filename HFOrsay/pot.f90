03e36018bc9b2cab9f69e32320e96988c68cfe30
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-07-20 18:40:15 +0200

    Minnesota potential added

--- HFOrsay/pot.f90
+++ HFOrsay/pot.f90
@@ -6,4 +6,12 @@ module pot
   double precision::r1,r2,v,mu,v0
   v=-(one*v0*(exp(2*mu*r1*r2)-exp(-2*mu*r1*r2))*exp(-mu*(r1**2+r2**2)))/(4.d0*r1*r2*mu)
   end function
+  function minnesota(r1,r2,vc,cc) result(res)
+  use constants
+  implicit none
+  double precision::r1,r2,vc,cc,res
+  res = vc/cc*half*exp(-cc*(r1**2+r2**2))*sinh(2*cc*r1*r2)
+  end function
+
+
 end module
