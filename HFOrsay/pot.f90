c2bc821cf8e3e7e16b411ac0c0202b4abe755b06
maxime mougeot <maxime.mougeot@cern.ch>
2016-07-20 14:34:22 +0100

    corrected integration error while calculating the potentail

--- HFOrsay/pot.f90
+++ HFOrsay/pot.f90
@@ -1,10 +1,9 @@
-module pot  
+module pot
   contains
   function potential(r1,r2,v0,mu) result(v)
   use constants
   implicit none
   double precision::r1,r2,v,mu,v0
-  v=-(one/4.d0*(1-exp(-2*mu*r1*r2))*exp(-mu*(r1**2+r2**2))*v0)/(r1*r2*mu)
+  v=-(one*v0*(exp(2*mu*r1*r2)-exp(-2*mu*r1*r2))*exp(-mu*(r1**2+r2**2)))/(4.d0*r1*r2*mu)
   end function
 end module
-
