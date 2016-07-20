d4d1e632227e214764f93ae08666ab3a5898fa93
Kyle Godbey <kylegodbey@gmail.com>
2016-07-20 18:04:29 +0100

    LR works for symmetric potentials, and we have woodsaxon

--- projects/FortranDefenders/grid.f95
+++ projects/FortranDefenders/grid.f95
@@ -4,11 +4,11 @@ implicit none
      real(wp), parameter :: pi = 3.14159265358979_wp
      real(wp) :: h,conv,hbar22m,v0
      real(wp), allocatable,dimension(:) :: meshpoints,wavefunctions,wfl,wfr
-     integer :: nbox, nodes
+     integer :: nbox, nodes, radius
 contains
      subroutine init_params
 
-          namelist /input/ nbox,h,nodes,v0,conv,hbar22m
+          namelist /input/ nbox,h,nodes,v0,radius,conv,hbar22m
           read(5,input)
 
      end subroutine init_params
@@ -23,7 +23,7 @@ contains
      end subroutine init_grids
 
      subroutine init_wavefunctions
-	
+
           allocate(wavefunctions(0:nbox),wfr(0:nbox),wfl(0:nbox))
 
      end subroutine
