4dfdc5374f6999d2f2ae8e0d5f91ad9b45e76b65
Pierre BECKER <becker_pierre@hotmail.fr>
2016-07-20 17:36:51 +0200

    Wood-Saxon, brushed, cleaned and working asymmetric and symmetric

--- projects/FortranDefenders/solver.f95
+++ projects/FortranDefenders/solver.f95
@@ -241,28 +241,6 @@ contains
 
   end function
 
- !function woodsaxon(ir) result(pot)
-  !  real(wp) :: pot
-  !  integer, intent(in) :: ir
-
-!    if (ir < nbox/2) then
-!      pot = v0 * 1 / (1 * exp((-meshpoints(nbox/2-ir)+meshpoints!!(radius))/0.67))
-!    else
-!      pot = v0 * 1 / (1 * exp((-meshpoints(ir-nbox/2)+meshpoints(radius))/0.67))
-!    end if
-
-
-!  end function
-
- function woodsaxons( r0, r ) result(res)
-    implicit none
-    real (wp), intent(in) :: r0, r
-    real (wp) :: res
-    !
-    res =  1 / ( 1 + exp( (   r - r0 ) / 0.67 ) )    &
-         + 1 / ( 1 + exp( ( - r - r0 ) / 0.67 ) ) - 1
-    !
-  end function woodsaxons
 
   function woodsaxon(ir) result(pot)
     real(wp) :: pot
