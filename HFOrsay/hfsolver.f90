6485d666dfcc651fd5820f5e791d0dc5e8707773
Raphaël-David Lasseri <lasseri@ipno.in2p3.fr>
2016-07-20 22:41:24 +0200

    Corrections of normalization factors

--- HFOrsay/hfsolver.f90
+++ HFOrsay/hfsolver.f90
@@ -15,7 +15,7 @@
       DOUBLE PRECISION, ALLOCATABLE :: eigvalR(:), eigvalL(:), eigvalOLD(:), WORK(:)
       DOUBLE PRECISION, ALLOCATABLE :: rho(:,:), vpot(:,:,:,:), kin(:,:), gama(:,:)
       DOUBLE PRECISION esum, rhosum, gamasum, vnorm
-      double precision::x
+      double precision::x,ri,rj
 !      external::compute_rho,compute_h,compute_gamma
 !
       EXTERNAL dgeev
@@ -54,18 +54,29 @@
 	 kin(i,i) = (2.d0*i+1.5d0)*ama*2.d0/(bosc**2)
       enddo
 
+do i=0,400
+ri = 0.1*i
+ do j=0,400
+ rj = 0.1*j
+write(11,*) ri,rj,potential(ri,rj,v0r,kr)
+write(12,*) ri,rj,minnesota(ri,rj,v0r,kr)
+enddo
+write(11,*)
+enddo
+
+
      do i = 1,n
       do j = 1,n
        do k = 1,n
         do l = 1,n
-           call tbme(n,i,j,k,l,vpot(i,j,k,l))
-           write(11,*) i,j,k,l,vpot(i,j,k,l)
+           call tbme((n+1)/2,i,j,k,l,vpot(i,j,k,l))
+           write(14,*) i,j,k,l,vpot(i,j,k,l)
         enddo !l
-           write(11,*) 
+           write(14,*) 
        enddo !k
-           write(11,*) 
+           write(14,*) 
       enddo !j
-           write(11,*) 
+           write(14,*) 
      enddo !i
 
 ! ---------- start of iteration loop
--- HFOrsay/tbme.f90
+++ HFOrsay/tbme.f90
@@ -29,21 +29,19 @@ do i=1,n
     wi=lag_w(i)
     xxi=(lag_zeros(i))
     xi = dsqrt(xxi)
-    coeffi =exp(xxi)*xxi**(0.d0)
+    coeffi =1.d0!exp(xxi)*xxi**(0.d0)
     osc1 = coeffi*ho_rad_wf(n1,0,xi)*ho_rad_wf(n3,0,xi)
  do j=1,n
     wj = lag_w(j)
     xxj=(lag_zeros(j))
     xj = dsqrt(xxj)
-    coeffj = exp(xxj)*xxj**(0.d0)
+    coeffj = 1.d0!exp(xxj)*xxj**(0.d0)
     osc2 = coeffj*ho_rad_wf(n2,0,xj)*ho_rad_wf(n4,0,xj)
-    inte1 = inte1 + osc1*osc2*potential(xxi,xxj,v0r,kr)*coeffi*coeffj
-    inte2 = inte2 + osc1*osc2*potential(xxi,xxj,v0s,ks)*coeffi*coeffj
+    inte1 = inte1 + xxi**2*xxj**2*osc1*osc2*potential(xxi,xxj,v0r,kr)*coeffi*coeffj
+    inte2 = inte2 + xxi**2*xxj**2*osc1*osc2*potential(xxi,xxj,v0s,ks)*coeffi*coeffj
   enddo !j
-    write(*,'(3f12.8)') xxi,xxj,minnesota(xxi,xxj,v0r,kr)
  !   read(*,*)
  enddo !i
- read(*,*)
 resu = -inte1 + inte2
-write(*,*) "Integral result",resu
+!write(*,*) "Integral result",resu
 end
