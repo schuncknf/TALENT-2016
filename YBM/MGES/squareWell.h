6ddbfede4cea866c7bc17f3f18d3c74797c6cc59
mges501York <mges501@york.ac.uk>
2016-07-21 23:26:18 +0100

    DLS condition function added

--- YBM/MGES/squareWell.h
+++ YBM/MGES/squareWell.h
@@ -12,21 +12,21 @@
 #include <sstream>
 #include <cmath>
 
-
-
 // Functions
-double numerov(double psi0, double psi1, double E_trial);
+double numerov(double x, double psi0, double psi1, double E_trial);
 double v(double x, double E_trial);
-double potential(double x);
+double potential(double x, int potType);
+double checkDLS(double E, double matchFraction);
 
 // System parameters
+extern double L; // box width
 extern double a; // well width
-extern double hbar2m; // h-bar/2m
-extern double h; // step size
-extern double epsilon; // precision
+extern double V0; // well depth
+extern int potType; // type of potential well
 
-// System variables
-extern double x; // position in well
+extern double hbar2m; // h-bar/2m
+extern double h; // mesh spacing
+extern double epsilon; // precision
 
 #endif
 #endif
