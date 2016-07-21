03f4815bcaa0761bd62dd600f6676122670c31e1
Pierre Arthuis <pierre.arthuis@cea.fr>
2016-07-21 15:16:31 +0200

    System header modified accordingly to UML diagram

--- projects/DamnDucksTowels/cpp/include/System.h
+++ projects/DamnDucksTowels/cpp/include/System.h
@@ -5,9 +5,19 @@ class System {
   // Attributes
 public:
   Basis* basis;
-  /// Number of neutrons
-  int nNumber;
-  /// number of proton
-  int pNumber;
+  /// Number of different particles
+  arma::ivec particleNumbers ;
+  /// Names of the different particle types
+  std::vector<std::string> particleNames ;
+  /// Type of potential used for calculations
+  int potType ;
+  /// Name of the system
+  std::string name = "unnamed" ;
+
+  // Methods
+  /// First guess of the Hamiltonian
+  virtual void calcH0 (arma::field<arma::mat> & H, int type) = 0;
+  /// Calculation of the Hamiltonian
+  virtual void calcH (arma::field<arma::mat> & H, arma::field<arma::mat> & RG) = 0;
 };
 
