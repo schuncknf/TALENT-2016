923a3642c020df91cd6529c2a9e93af95abedf1f
Kyle Godbey <kylegodbey@gmail.com>
2016-07-26 15:31:22 +0100

    Left right method is implemented. Finally..

--- projects/FortranDefenders/WoodSaxon/main.f90
+++ projects/FortranDefenders/WoodSaxon/main.f90
@@ -1,7 +1,7 @@
 program main
     use grid
     use solver
-    integer :: iq,i,npr,ir
+    integer :: iq,i,npr,ir,n,l
     real(wp) :: j
     open(unit=5,file='in',status='old',form='formatted')
     open(unit=6,file='out',form='formatted')
@@ -12,7 +12,7 @@ program main
     call init_wavefunctions
 
     call solve_r
-    call energy_sort
+
    ! do i = 1,nmax
    !  print *, sortenergies(i,1), sortstates(i,1,1), &
    !  &       sortstates(i,2,1),sortstates(i,3,1)
@@ -32,6 +32,25 @@ program main
 !      end do
 !    end do
 !    write(6,*) "Total Bound States =", sum(vocc(1:lmax-2,:,:,:)), "Vpb(1) =", vpb(1)
+  do iq =1,2
+    density(:,iq) = 0.
+    do n=1,lmax-2
+      do l=0,lmax
+        do is=1,2
+          if(energies(n,l,is,iq) < 0 .AND. energies(n,l,is,iq) > vpb(iq)+.01) then
+
+            j = l + spin(is)
+            if (l == 0) j = 0
+            do ir = 0, nbox
+              density(ir,iq) = density(ir,iq) + (2*j+1)*wfr(ir,n,l,is,iq) &
+              *wfr(ir,n,l,is,iq) / (4*pi*meshpoints(ir)**2)
+            end do
+          end if
+        end do
+      end do
+    end do
+  end do
+  call energy_sort
   write(6,*) "Single Particle States | "
     do iq =1,2
 
@@ -48,8 +67,8 @@ program main
               j = sortstates(i,2,iq) + spin(sortstates(i,3,iq))
               if (sortstates(i,2,iq) == 0) j = 0
               do ir=0,nbox
-                density(ir,iq) = density(ir,iq) + (2*j+1)*wfr(ir,sortstates(i,1,iq),sortstates(i,2,iq),sortstates(i,3,iq),iq)&
-                *wfr(ir,sortstates(i,1,iq),sortstates(i,2,iq),sortstates(i,3,iq),iq) / (4*pi*meshpoints(ir)**2)
+                !density(ir,iq) = density(ir,iq) + (2*j+1)*wfr(ir,sortstates(i,1,iq),sortstates(i,2,iq),sortstates(i,3,iq),iq)&
+                !*wfr(ir,sortstates(i,1,iq),sortstates(i,2,iq),sortstates(i,3,iq),iq) / (4*pi*meshpoints(ir)**2)
               end do
               write (6,*) "n=", sortstates(i,1,iq), &
                           &"l=",sortstates(i,2,iq),&
@@ -60,9 +79,11 @@ program main
       end do
     end do
 
-  write(6,*) "Total Bound States =", sum(vocc(1:lmax-2,:,:,:)), "Vpb(1) =", vpb(1)
+  write(6,*) "Total Neutrons =", sum(4*pi*h*meshpoints(:)**2 * density(:,1)), &
+  "Total Protons =", sum(4*pi*h*meshpoints(:)**2 *density(:,2))
     do ir=0,nbox
-      write (13,*) ir*h, wfr(ir,3,2,1,1),density(ir,1), density(ir,2)
+      write (13,*) ir*h, wfr(ir,3,0,1,1), wfr(ir,3,1,1,1), wfr(ir,3,2,1,1)
+      !wfr(ir,1,3,1,1), wfr(ir,1,4,1,1), wfr(ir,1,5,1,1), wfr(ir,1,6,1,1)
     end do
     close(13)
     close(6)
