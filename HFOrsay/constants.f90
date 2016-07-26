deaffcb8418236711c4fbb85eed9d317757e3be3
nom <ubuntu@ubuntu.(none)>
2016-07-26 17:06:06 +0100

    size of basis

--- HFOrsay/constants.f90
+++ HFOrsay/constants.f90
@@ -32,7 +32,7 @@ read(1,'(10x,i5)') maxit
 read(1,'(10x,f10.4)') homega
 nbase = nbase
 ntx = (nbase + 2)*(nbase +3)
-ntx = ntx/2
+ntx = ntx/2-1
 bosc = dsqrt(2.d0*ama/homega)
 
 write(*,*) "******** READER *************"
