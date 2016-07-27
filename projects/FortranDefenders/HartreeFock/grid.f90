346a180d481fe215767b2b0fb1150c30626c79f4
Kyle Godbey <kylegodbey@gmail.com>
2016-07-27 13:22:19 +0100

    New development directory for HF

---
+++ projects/FortranDefenders/HartreeFock/grid.f90
@@ -0,0 +1,61 @@
+module grid
+implicit none
+     integer, parameter :: wp=kind(1.0d0)
+     real(wp), parameter :: pi = 3.14159265358979_wp
+     real(wp), parameter :: e2 = 1.439646_wp
+     real(wp), parameter :: hbar = 6.582119E-22_wp
+     real(wp), parameter :: a = 0.67_wp
+     real(wp), parameter :: vso = 23_wp
+     real(wp) :: h,conv,hbar22m,v0,nrad,vpb(2),r0,small,spin(2)
+     real(wp), allocatable,dimension(:) :: meshpoints
+     real(wp), allocatable, dimension(:,:) :: rho
+     real(wp), allocatable, dimension(:,:,:,:,:) :: wavefunctions,wfl,wfr
+     integer :: nbox, nodes, radius, lmax, welltype,nmax,njoin
+     integer :: nn,np,nt
+contains
+     subroutine init_params
+
+          namelist /box/ nbox,h
+          namelist /squarewell/ welltype,nodes,v0,radius
+          namelist /params/ r0,conv,hbar22m
+          namelist /nucleus/ nn,np,lmax
+          read(5,box)
+          read(5,squarewell)
+          read(5,params)
+          read(5,nucleus)
+          if ((welltype /= 1) .AND. (welltype /= 2)) write (*,*) "Put in a proper welltype!"
+          nt = np+nn
+          nrad = r0 * (nt)**(1._wp/3._wp)
+          vpb(1) = -51.+33.*(nn-np)/nt
+          vpb(2) = -51.-33.*(nn-np)/nt
+          spin(1) = -0.5
+          spin(2) = 0.5
+          njoin = 200
+          nmax = nn - np
+
+          if (nmax.ge.0) then
+          nmax = nn
+          else
+          nmax = np
+          end if
+
+     end subroutine init_params
+
+     subroutine init_grids
+
+          integer :: i
+          small = 1E-20_wp
+          allocate(meshpoints(0:nbox))
+          meshpoints = (/ (real(i)*h,i=0,nbox) /)
+          meshpoints(0) = small
+
+     end subroutine init_grids
+
+     subroutine init_wavefunctions
+
+          allocate(wavefunctions(0:nbox,lmax,0:lmax,2,2),wfr(0:nbox,lmax,0:lmax,2,2),wfl(0:nbox,lmax,0:lmax,2,2),rho(0:nbox,4))
+
+     end subroutine
+
+
+end module grid
---
+++ projects/FortranDefenders/HartreeFock/main.f90
@@ -0,0 +1,51 @@
+program main
+    use grid
+    use solver
+    integer :: iq,i,npr,ir,n,l
+    real(wp) :: j
+    open(unit=5,file='in',status='old',form='formatted')
+    open(unit=6,file='out',form='formatted')
+    open(unit=13,file='plt',form='formatted')
+    open(unit=14,file='densities',form='formatted')
+
+    call init_params
+    call init_grids
+    call init_wavefunctions
+    call solve_r
+    call energy_sort
+    call build_densities
+    ! Wriing the single particle states to 'out'
+    write(6,*) "Single Particle States | "
+    do iq =1,2
+
+      if (iq == 1) then
+      write(6,*) "Neutrons | "
+      npr = nn
+      else
+      write(6,*) "Protons  | "
+      npr = np
+      end if
+      do i = 1, npr
+            if (sortenergies(i,iq) < - small) then
+              j = sortstates(i,2,iq) + spin(sortstates(i,3,iq))
+              if (sortstates(i,2,iq) == 0) j = 0
+              write (6,*) "n=", sortstates(i,1,iq), &
+                          &"l=",sortstates(i,2,iq),&
+                          &"is=",sortstates(i,3,iq),&
+                          &"Energy=", sortenergies(i,iq)
+            end if
+      end do
+    end do
+    do ir=0,nbox
+      write (13,*) ir*h, vpb(1)*fullwoodsaxon(ir), vpb(2)*fullwoodsaxon(ir)+coulomb(ir), wfr(ir,2,3,1,1), wfr(ir,3,1,1,1)
+    end do
+    ! Particle by way of density integration
+    write(6,*) "Total Neutrons =", sum(4*pi*h*meshpoints(:)**2*rho(:,1)), &
+    "Total Protons =", sum(4*pi*h*meshpoints(:)**2 *rho(:,2))
+
+    close(13)
+    close(6)
+    close(5)
+    close(14)
+
+end program main
