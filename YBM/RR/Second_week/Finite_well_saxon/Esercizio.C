cfea10f2de9c6728d2e829cbda06aa24695b1c89
Rykart <riccardo.romano11@gmail.com>
2016-07-28 17:49:35 +0200

    Density_works

--- YBM/RR/Second_week/Finite_well_saxon/Esercizio.C
+++ YBM/RR/Second_week/Finite_well_saxon/Esercizio.C
@@ -6,18 +6,18 @@ int main(){
 //Finite well
 
 	double Edown = 0.;
-	double Eup = 100.;
-	double n_step_width_box = width_box/h_width;
+	double Eup = -50.;
+	int n_step_width_box = width_box/h_width;
 
 	vector<double> wave_val;
 	vector<double> eigenval;
 	
 
 //Routine to find eigevalues: this works finding the value of energy at which the eigenfunction cross the 0 
-	
+/*	
 	double S = 1./2.;
 	cout<<"Level"<<"    	"<<"L value"<<"    	"<<"J value"<<"  	  	"<<"Eigenvalue"<<endl;
-	for(int node=0; node<4; node++){
+	for(int node=0; node<3; node++){
 		for(int L=0; L<8;L++){
 			for(int h=1; h<3; h++){
 				if(L==0) h=2;
@@ -25,16 +25,16 @@ int main(){
 				int nodecount=0;
 				double Etrial=0.;
 				Eup = 0.;
-				Edown = -100.;
+				Edown = -50.;
 				do{
 					Etrial = (Eup+Edown)/2.;
-					wave_val.push_back(0.);
-					wave_val.push_back(0.05);
+					wave_val.push_back(0.1);
+					wave_val.push_back(0.15);
 					for(int i=1; i<n_step_width_box; i++){
-						wave_val.push_back(numerov_algorithm_woods(Etrial, wave_val[i], wave_val[i-1], i*h_width+h_width,S,L,J));
+						wave_val.push_back(numerov_algorithm_woods(Etrial, wave_val[i], wave_val[i-1], i*h_width+2.*h_width,S,L,J));
 					}
 					nodecount = 0;
-					for(int i=1; i<=n_step_width_box; i++){
+					for(int i=1; i<n_step_width_box+2; i++){
 						if(wave_val[i-1]*wave_val[i]<0.)
 						nodecount++;
 					}
@@ -46,13 +46,79 @@ int main(){
 					}
 					wave_val.clear();
 				}while(abs(Eup-Edown)>prec);
-			if(Etrial<-0.1&&Etrial>-80)
-			cout<<node+1<<"	  	"<<L<<"    	 	"<<J*2<<"/2"<<"  	 		"<<Etrial<<endl;
+			if(Etrial<-4.5&&Etrial>-50)
+			cout<<node+1<<"	  	"<<L<<"    	 	"<<J*2<<"/2"<<"  	 		"<<setprecision(12)<<Etrial<<endl;
 			}
 		}
 		cout<<endl;
 	}
+*/
 
+
+//Density
+	ofstream myfile_density, myfile_wave;
+	char orbital[26];
+
+	double * density = new double [n_step_width_box+1];
+	for(int i=1; i<n_step_width_box+1; i++){
+		density[i] = 0.;
+	}
+	vector<double> wave_eigen;
+	double S = 1./2.;
+	for(int node=0; node<3; node++){
+		for(int L=0; L<8;L++){
+			for(int h=1; h<3; h++){
+				if(L==0) h=2;
+				double J = abs(S+pow(-1,h)*L);
+				int nodecount=0;
+				double Etrial=0.;
+				Eup = 0.;
+				Edown = -50.;
+				do{
+					Etrial = (Eup+Edown)/2.;
+					wave_val.push_back(0.1);
+					wave_val.push_back(0.15);
+					for(int i=1; i<n_step_width_box; i++){ //the vector has n_step_width_box + 1 boxes
+						wave_val.push_back(numerov_algorithm_woods(Etrial, wave_val[i], wave_val[i-1], i*h_width+2.*h_width,S,L,J));
+					}
+					nodecount = 0;
+					for(int i=1; i<n_step_width_box+1; i++){
+						if(wave_val[i-1]*wave_val[i]<0.)
+						nodecount++;
+					}
+					if(nodecount>node){
+						Eup = Etrial;
+					}
+					else if(nodecount<node||nodecount==node){
+						Edown = Etrial;
+					}
+					wave_val.clear();
+				}while(abs(Eup-Edown)>prec);
+				if(Etrial<-4.5&&Etrial>-50){
+					wave_eigen.push_back(0.1);
+					wave_eigen.push_back(0.15);
+					sprintf(orbital,"wf_%1.4f_.dat",Etrial);
+					myfile_wave.open(orbital);
+					cout<<Etrial<<"	";
+					double norm = normalise(Etrial, n_step_width_box, S, L, J);
+					cout <<norm<<endl;
+					for(int i=1; i<n_step_width_box+1; i++){
+						wave_eigen.push_back(numerov_algorithm_woods(Etrial, wave_eigen[i], wave_eigen[i-1], i*h_width+2.*h_width,S,L,J));
+						density[i] += (2.*J+1.)/(4.*M_PI*pow(i*h_width+2.*h_width,2))*pow(wave_eigen[i]/sqrt(norm),2);
+						myfile_wave << i*h_width +2.*h_width<< "   " << wave_eigen[i]/(sqrt(norm)*(i*h_width +2.*h_width)) << endl;	//Crea un numero di file pari agli autovalori e guarda nelle f d'onda dove ci sono i problemi ;-)
+					}
+					wave_eigen.clear();
+					myfile_wave.close();
+				}
+			}
+		}	
+	}
+
+	myfile_density.open("Density.txt");
+	for(int i=1; i<n_step_width_box+1; i++){
+		myfile_density << i*h_width +2.*h_width<< "   " << density[i] << endl;
+	}
+	myfile_density.close();
 
 // Write results to file with filename
 /*
@@ -71,15 +137,15 @@ int main(){
 	myfile.close();
 */
 
-
+//a=system('a=`tempfile`;cat *.dat > $a;echo "$a"') 
 //Plot the potential - Works
 /*
 	ofstream myfile;
 	myfile.open("Eigen.txt");
 
 	for(int i=0; i<n_step_width_box; i++){
-		wave_val.push_back(potential_woods(i*h_width+2*h_width)+potential_spin_orbit(i*h_width+2*h_width,S, L)+potential_coulomb(i*h_width+2*h_width));
-		myfile <<i*h_width +2*h_width<< "   " << wave_val[i] << endl;
+		wave_val.push_back(potential_woods(i*h_width +h_width)-centrifug_term(i*h_width +h_width, 0));
+		myfile <<i*h_width +h_width<< "   " << wave_val[i] << endl;
 	}
 
 	myfile.close();
--- YBM/RR/Second_week/Finite_well_saxon/oggetto.C
+++ YBM/RR/Second_week/Finite_well_saxon/oggetto.C
@@ -1,7 +1,16 @@
 #include"Esercizio.h"
 
 	double numerov_algorithm_woods(double energy, double f0, double f_,double r, double S, double L, double J){
-		double v = (energy-potential_woods(r)-potential_spin_orbit(r,S,L,J))/m_factor-centrifug_term(r,L);
+		double v = (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J))/m_factor;
+		double a[3];
+		a[0] = 2. * (1. - 5./12. * v * pow(h_width,2));	// Coeff. for f(x)
+		a[1] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x-h)
+		a[2] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x+h)
+		return (a[0] * f0 - a[1] * f_) / a[2];
+	}
+
+	double numerov_algorithm_woods_proton(double energy, double f0, double f_,double r, double S, double L, double J){
+		double v = (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J)-potential_coulomb(r))/m_factor;
 		double a[3];
 		a[0] = 2. * (1. - 5./12. * v * pow(h_width,2));	// Coeff. for f(x)
 		a[1] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x-h)
@@ -11,13 +20,13 @@
 
 
 	double potential_woods(double r){
-		double V = -51+33*(N-Z)/A; 
+		double V = -51.+33.*(N-Z)/(N+Z); 
 		double R = r0*pow(A,1./3.);
 		return V/(1+exp((r-R)/0.67));
 	}
 
 	double centrifug_term(double r, double L){
-		return L*(L+1)/pow(r,2);
+		return m_factor*L*(L+1)/pow(r,2);
 	}
 
 	double potential_coulomb(double r){
@@ -30,11 +39,25 @@
 
 	double potential_spin_orbit(double r, double S, double L, double J){
 		if(L!=0){
+			double V = -51.+33.*(N-Z)/(N+Z); 
 			double R = r0*pow(A,1./3.);
-			double S_L = J*(J+1)-L*(L-1)-3./4.;
-			double df = -1./pow(1+exp((r-R)/0.67),2)*1./0.67*exp((r-R)/0.67);
-			return 0.44*S_L*pow(r0,2)*1./r*df;
+			double S_L = 1./2.*(J*(J+1)-L*(L+1)-3./4.);
+			double df = -1./(pow(1+exp((r-R)/0.67),2))*(1./0.67)*exp((r-R)/0.67);
+			return -0.22*V*1./r*df*S_L*pow(r0,2);
 		}
 		else
 			return 0;
 	}
+
+	double normalise(double eigen, double n_step_width_box, double S, double L, double J){
+	vector<double> wfWork;
+	wfWork.push_back(0.1);
+	wfWork.push_back(0.15);
+
+	double sum = 0.;
+	for(int i=1; i<n_step_width_box+1; i++) wfWork.push_back(numerov_algorithm_woods(eigen, wfWork[i], wfWork[i-1], i*h_width + 2*h_width, S, L, J));
+	for(int i=1; i<n_step_width_box+1; i++) sum += 4*M_PI*h_width*pow(wfWork[i],2);
+
+	wfWork.clear();
+	return sum;
+}
