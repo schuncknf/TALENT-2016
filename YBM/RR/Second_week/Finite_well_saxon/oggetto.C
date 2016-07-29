bae22c02a73ef62964bb329a008858e600c5add5
Rykart <riccardo.romano11@gmail.com>
2016-07-29 17:37:56 +0200

    Densities and all that stuff

--- YBM/RR/Second_week/Finite_well_saxon/oggetto.C
+++ YBM/RR/Second_week/Finite_well_saxon/oggetto.C
@@ -1,23 +1,28 @@
 #include"Esercizio.h"
 
-	double numerov_algorithm_woods(double energy, double f0, double f_,double r, double S, double L, double J){
-		double v = (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J))/m_factor;
-		double a[3];
-		a[0] = 2. * (1. - 5./12. * v * pow(h_width,2));	// Coeff. for f(x)
-		a[1] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x-h)
-		a[2] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x+h)
-		return (a[0] * f0 - a[1] * f_) / a[2];
+	double numerov_algorithm_woods(double energy, double f0, double f_,double r, double S, double L, double J, float T){
+		double v=0., a[3];
+		if(T==-1./2.){
+			a[0] = 2. * (1. - 5./12. * v_neutron(energy,r,S,L,J) * pow(h_width,2));	// Coeff. for f(x)
+			a[1] = 1. * (1. + 1./12. * v_neutron(energy,r-h_width ,S,L,J) * pow(h_width,2));	// Coeff. for f(x-h)
+			a[2] = 1. * (1. + 1./12. * v_neutron(energy,r+h_width,S,L,J) * pow(h_width,2));	// Coeff. for f(x+h)
+			return (a[0] * f0 - a[1] * f_) / a[2];
+		}
+		else if(T==1./2.){
+			a[0] = 2. * (1. - 5./12. * v_proton(energy,r,S,L,J) * pow(h_width,2));	// Coeff. for f(x)
+			a[1] = 1. * (1. + 1./12. * v_proton(energy,r-h_width,S,L,J) * pow(h_width,2));	// Coeff. for f(x-h)
+			a[2] = 1. * (1. + 1./12. * v_proton(energy,r+h_width,S,L,J) * pow(h_width,2));	// Coeff. for f(x+h)
+			return (a[0] * f0 - a[1] * f_) / a[2];
+		}
 	}
 
-	double numerov_algorithm_woods_proton(double energy, double f0, double f_,double r, double S, double L, double J){
-		double v = (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J)-potential_coulomb(r))/m_factor;
-		double a[3];
-		a[0] = 2. * (1. - 5./12. * v * pow(h_width,2));	// Coeff. for f(x)
-		a[1] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x-h)
-		a[2] = 1. * (1. + 1./12. * v * pow(h_width,2));	// Coeff. for f(x+h)
-		return (a[0] * f0 - a[1] * f_) / a[2];
+	double v_neutron(double energy, double r,double S, double L, double J){
+		return (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J))/m_factor;
 	}
 
+	double v_proton(double energy, double r,double S, double L, double J){
+		return (energy-potential_woods(r)-centrifug_term(r,L)-potential_spin_orbit(r,S,L,J)-potential_coulomb(r))/m_factor;
+	}
 
 	double potential_woods(double r){
 		double V = -51.+33.*(N-Z)/(N+Z); 
@@ -32,9 +37,9 @@
 	double potential_coulomb(double r){
 		double R = r0*pow(A,1./3.);
 		if(r<=R)
-			return Z*pow(e,2)/(2.*R)*(3-pow(r/R,2));
+			return Z*e/(2.*R)*(3-pow(r/R,2));
 		else
-			return Z*pow(e,2)/r;
+			return Z*e/r;
 	}
 
 	double potential_spin_orbit(double r, double S, double L, double J){
@@ -49,14 +54,14 @@
 			return 0;
 	}
 
-	double normalise(double eigen, double n_step_width_box, double S, double L, double J){
+	double normalise(double eigen, double n_step_width_box, double S, double L, double J, float T){
 	vector<double> wfWork;
+	wfWork.push_back(0.05);
 	wfWork.push_back(0.1);
-	wfWork.push_back(0.15);
 
 	double sum = 0.;
-	for(int i=1; i<n_step_width_box+1; i++) wfWork.push_back(numerov_algorithm_woods(eigen, wfWork[i], wfWork[i-1], i*h_width + 2*h_width, S, L, J));
-	for(int i=1; i<n_step_width_box+1; i++) sum += 4*M_PI*h_width*pow(wfWork[i],2);
+	for(int i=1; i<n_step_width_box+1; i++) wfWork.push_back(numerov_algorithm_woods(eigen, wfWork[i], wfWork[i-1], i*h_width + h_width, S, L, J, T));
+	for(int i=1; i<n_step_width_box+1; i++) sum += h_width*pow(wfWork[i],2);
 
 	wfWork.clear();
 	return sum;
