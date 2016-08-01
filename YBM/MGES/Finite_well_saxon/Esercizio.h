392209d18130b49ef77ee414fc3fc03744eba133
mges501York <mges501@york.ac.uk>
2016-08-01 17:22:49 +0100

    MGES version of Ricardo's code

--- YBM/MGES/Finite_well_saxon/Esercizio.h
+++ YBM/MGES/Finite_well_saxon/Esercizio.h
@@ -5,25 +5,46 @@
 	#include <cmath>
 	#include <vector>
 	#include <fstream>
+	#include <iomanip>
+	#include <sstream>
+	#include <algorithm>
+	
 
-	#define width_box 34.
-	#define width 6.
-	#define h_width 0.001
-	#define V0 40.
+	#define width_box 22.
+	#define h_width 0.01
+	#define r0 1.27
+	#define e 1.439978
+	
+	#define A 208.
+	#define N 126.
+	#define Z 82.
 
-	#define prec 1E-10
-	#define m_factor 20.75
-    
-    #define r0 1.27
-    #define a_comp 0.67
-    #define Vls 0.44
-    
-    #define nProton 82
-    #define nNeutron 126
+	#define prec 1E-12
+	#define m_factor 20.73553
 
 	using namespace std;
 
-	double numerov_algorithm(double energy, double f_, double f0, double x, int potType, int l, double s);
-    double potential(double x, int potType, int l, double s);
+	double numerov_algorithm_woods(double energy, double f0, double f_,double r,double S, double L, double J, float T);
+	double v_neutron(double energy, double r,double S, double L, double J);
+	double v_proton(double energy, double r,double S, double L, double J);
+	double centrifug_term(double r, double L);
+	double potential_woods(double r);
+	double potential_spin_orbit(double r, double S, double L, double J);
+	double potential_coulomb(double r);
+	double normalise(double eigen, double n_step_width_box, double S, double L, double J, float T);
+    double particleFromDensity(double * density,double n_step_width_box);
+
+	struct state{
+		vector<double> wavefn;
+    	double n, l, j;
+    	double eig;
+	};
+	
+	struct compare{
+   		bool operator() (const state struct1, const state struct2){
+   	    	return (struct1.eig < struct2.eig);
+   		}
+   	};
+
 
 #endif
