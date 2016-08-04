2aab54dd2e1126fd91b3187d2c75155b29757dc0
cgonzalezboquera <cgonzalezboquera@ub.edu>
2016-08-04 16:44:56 +0200

    Not Coulomb corrected

--- PeTsBav2/WEEK3/t0_t3/arrange.f90
+++ PeTsBav2/WEEK3/t0_t3/arrange.f90
@@ -1,9 +1,19 @@
+!!  Subroutine to arrange the HF eigenvalues in ascending order
+!!
+!!*******************************************************************************
+!! E    : matrix that contains the values of the eigenvalues in the first column,
+!!        and the quantum numbers in the other columns
+!! dim  : number of orbitals calculated
+!! func : matrix with the wavefunctions stored
+!! N    : dimension of the mesh
+!!********************************************************************************
 subroutine arrange(E, dim, func, N)
 implicit none
+integer :: i, j, dim, N
 real(8) :: E(dim,4), box(4)
 real(8) :: func(0:N,1:dim), func_tmp(0:N)
 real(8) :: number(dim)
-integer :: i, j, dim, N
+
 
 do i=1,dim-1
    do j=1,dim-i
